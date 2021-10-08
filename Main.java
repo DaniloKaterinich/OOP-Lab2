2d3586cc9c0914a9e973b987e74e6efe6905d8f9
KaterinichDanilo <danilokaterinich@gmail.com>
2021-10-08 18:02:43 +0300

    Task5

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static int Add(String num){
+    public static int Add(String num) throws Exception{
         int result = 0;
         if (num.length() == 0){
             return 0;
@@ -21,11 +21,29 @@ public class Main {
                 String [] numbersLine = Line.split(delimeter + "|,|\\\\n");
 
                 for (int i = 0; i < numbersLine.length; i++){
+                    if(Integer.parseInt(numbersLine[i]) < 0){
+                        StringBuffer nigga = new StringBuffer("");
+                        for (int j = 0; j < numbersLine.length; j++) {
+                            if (Integer.parseInt(numbersLine[j]) < 0) {
+                                nigga.append(numbersLine[j]);
+                            }
+                        }
+                        throw new Exception("negatives not allowed: " + nigga);
+                    }
                     result += Integer.parseInt(numbersLine[i]);
                 }
             } else{
                 String [] numbersLine = num.split(",|\\\\n");
                 for (int i = 0; i < numbersLine.length; i++){
+                    if(Integer.parseInt(numbersLine[i]) < 0){
+                        StringBuffer nigga = new StringBuffer("");
+                        for (int j = 0; j < numbersLine.length; j++) {
+                            if (Integer.parseInt(numbersLine[j]) < 0) {
+                                nigga.append(numbersLine[j]);
+                            }
+                        }
+                        throw new Exception("negatives not allowed: " + nigga.toString());
+                    }
                     result += Integer.parseInt(numbersLine[i]);
                 }
             }
@@ -39,7 +57,11 @@ public class Main {
         System.out.println("Введіть рядок: ");
         String num = in.nextLine();
         in.close();
-        System.out.println("result: " + Add(num));
+        try{
+            System.out.println("result " + Add(num));
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
