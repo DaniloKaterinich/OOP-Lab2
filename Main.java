37f79283936664a042ed77d09a9910e4a83ef2d9
KaterinichDanilo <danilokaterinich@gmail.com>
2021-10-08 11:27:47 +0300

    Task3

--- Main.java
+++ Main.java
@@ -2,23 +2,26 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    public static int Add(String num){
+        int result = 0;
+        if (num.length() == 0){
+            return 0;
+        } else{
+            String [] numbersLine = num.split(",|\\\\n");
+            System.out.println(numbersLine[0] + " " + numbersLine[1] + " " + numbersLine[2]);
+            for (int i = 0; i < numbersLine.length; i++){
+                result += Integer.parseInt(numbersLine[i]);
+            }
+            return result;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введіть рядок: ");
         String num = in.nextLine();
         in.close();
-
-        if (num.length() == 0){
-            System.out.println(0);
-        } else{
-            String [] numbers = num.split(",");
-            int result = 0;
-            for (int i = 0; i < numbers.length; i++){
-                result += Integer.parseInt(numbers[i]);
-            }
-            System.out.println(result);
-        }
+        System.out.println(Add(num));
 
     }
 }
